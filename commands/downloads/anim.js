ba5714c629c1ed0eb2f237d0ca0d29ea7cd42953
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-27 22:35:00 +0700

    feat(command): add anim command using WAIFU_API_URL from env

---
+++ commands/downloads/anim.js
@@ -0,0 +1,18 @@
+const { privat } = require("../../utils/helper");
+require("dotenv").config();
+const axios = require("axios");
+
+module.exports = {
+  name: "anim",
+  description: "Get a random waifu image from the API",
+  async execute(bot, msg) {
+    const chatId = msg.chat.id;
+    if (!privat(chatId)) return;
+    try {
+      const response = await axios.get(`${process.env.WAIFU_API}/sfw/waifu`);
+      bot.sendPhoto(chatId, response.data.url);
+    } catch (error) {
+      bot.sendMessage(chatId, "Failed to retrieve data.");
+    }
+  },
+};
