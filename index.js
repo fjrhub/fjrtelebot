33df72a7d78d3a8101c9609724bcb7f959848bf8
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-28 10:23:08 +0700

    chore(index): register auto download feature and toggle handlers

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
-require('module-alias/register');
-const { handleCommand, handleCallback } = require("@/handler");
+require("module-alias/register");
 require("dotenv").config();
 
-const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const { handleCallback, handleMessage } = require("@/handler");
 
-bot.on("message", (msg) => {handleCommand(bot, msg);});
-bot.on("callback_query", (query) => {handleCallback(bot, query);});
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+bot.on("message", (msg) => handleMessage(bot, msg));
+bot.on("callback_query", (query) => handleCallback(bot, query));
