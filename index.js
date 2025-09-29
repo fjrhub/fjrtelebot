c20a1a8b261589123a6ab2529d8a4baefd07926d
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-29 12:45:56 +0700

    feat(startup): notify owner when bot goes online with WIB timestamp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("module-alias/register");
 require("dotenv").config();
+const { OWNER_ID, getWIBTime } = require("@/utils/helper");
 
 const { handleCallback, handleMessage } = require("@/handler");
 
@@ -8,3 +9,4 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.on("message", (msg) => handleMessage(bot, msg));
 bot.on("callback_query", (query) => handleCallback(bot, query));
+bot.sendMessage(OWNER_ID,`Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
