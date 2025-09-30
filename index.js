40b6aeea5739218883dbc3f779bd01f9fb709ff8
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-30 09:36:42 +0700

    chore: start fresh migration to grammY

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const TelegramBot = require("node-telegram-bot-api");
-require("module-alias/register");
+const { Bot } = require("grammy");
 require("dotenv").config();
-const { OWNER_ID, getWIBTime } = require("@/utils/helper");
+const { OWNER_ID, getWIBTime } = require("./utils/helper");
 
-const { handleCallback, handleMessage } = require("@/handler");
+const bot = new Bot(process.env.TOKEN);
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
-
-bot.on("message", (msg) => handleMessage(bot, msg));
-bot.on("callback_query", (query) => handleCallback(bot, query));
-bot.sendMessage(OWNER_ID,`Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
+bot.api.sendMessage(OWNER_ID, `Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
