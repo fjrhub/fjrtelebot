32ce7ce3017f3cabb12433abcd41901789801095
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-30 09:38:00 +0700

    chore(index): initialize grammY bot and start event listeners

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 require("module-alias/register");
 const { Bot } = require("grammy");
 const { OWNER_ID, getWIBTime } = require("@/utils/helper");
+const { handleCallback, handleMessage } = require("@/handler");
 
 const bot = new Bot(process.env.BOT_TOKEN);
-
+bot.on("message", ctx => handleMessage(ctx));
+bot.on("callback_query:data", ctx => handleCallback(ctx));
 bot.api.sendMessage(OWNER_ID, `Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
+bot.start();
