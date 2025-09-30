328ccee4a4fc79ba7c42be4dcf8eb0a52193cf56
fjrhub <muhammadfajrulfalah@proton.me>
2025-09-30 09:37:04 +0700

    refactor: rename TOKEN to BOT_TOKEN for clarity

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Bot } = require("grammy");
 require("dotenv").config();
-const { OWNER_ID, getWIBTime } = require("./utils/helper");
+require("module-alias/register");
+const { Bot } = require("grammy");
+const { OWNER_ID, getWIBTime } = require("@/utils/helper");
 
-const bot = new Bot(process.env.TOKEN);
+const bot = new Bot(process.env.BOT_TOKEN);
 
 bot.api.sendMessage(OWNER_ID, `Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
