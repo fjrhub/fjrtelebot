4a90803ae4d4bb3c9ff5a1e7f3ca200e92a5ad4c
fjrhub <muhammadfajrulfalah@proton.me>
2025-10-01 22:40:09 +0700

    refactor(commands): simplify API call by removing tools prefix

--- commands/entertainment/asahotak.js
+++ commands/entertainment/asahotak.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const { isAuthorized } = require("@/utils/helper");
 const { setGame, getGame, clearGame } = require("@/utils/games");
-const tools = require("@/utils/api");
 
 module.exports = {
   name: "asahotak",
@@ -36,7 +35,7 @@ module.exports = {
     }
 
     try {
-      const res = await axios.get(tools.createUrl("siputzx", "/api/games/asahotak"), {
+      const res = await axios.get(createUrl("siputzx", "/api/games/asahotak"), {
         timeout: 8000,
       });
 
