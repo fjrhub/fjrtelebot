3b8eace275a6e85e27c98864456c2ad3e5a0e25d
fjrhub <muhammadfajrulfalah@proton.me>
2025-10-04 14:24:03 +0700

    refactor: clean up index.js by removing development-only console logs

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const { Bot } = require("grammy");
 const { OWNER_ID, getWIBTime } = require("@/utils/helper");
 const { handleCallback, handleMessage } = require("@/handler");
 
-if (process.env.NODE_ENV === "development") {
-  console.log("Running in development mode...");
-  console.log("Debug log active 🚀");
-}
-
 const bot = new Bot(process.env.TOKEN);
 bot.on("message", (ctx) => handleMessage(ctx));
 bot.on("callback_query:data", (ctx) => handleCallback(ctx));
