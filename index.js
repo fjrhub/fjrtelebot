0fb86ff6d663002bac8e8191ba42909ade6d096b
fjrhub <muhammadfajrulfalah@proton.me>
2025-10-30 21:43:55 +0700

    feat(deno): add basic Deno support with simple /ping command

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
-require("module-alias/register");
-require("@/utils/api");
-const { Bot } = require("grammy");
-const { OWNER_ID, getWIBTime } = require("@/utils/helper");
-const { handleCallback, handleMessage } = require("@/handler");
+import "jsr:@std/dotenv/load";
+import { Bot } from "npm:grammy";
 
-const bot = new Bot(process.env.TOKEN);
-bot.on("message", (ctx) => handleMessage(ctx));
-bot.on("callback_query:data", (ctx) => handleCallback(ctx));
-bot.api.sendMessage(OWNER_ID,`Bot is now online!\nStarted at: ${getWIBTime()} WIB`);
-bot.start({ timeout: 1800 });
+const TOKEN = Deno.env.get("TOKEN");
+const bot = new Bot(TOKEN);
+
+bot.command("ping", (ctx) => ctx.reply("🏓 Pong!"));
+bot.start();
+
+console.log("🤖 Bot is running on Deno...");
